d17fced24b3396ac6e78a4eaf80d8a5bec55369c
Maximilian Schöll <max@schoell.co>
2021-06-02 10:22:09 +0200

    doc: comment methods

--- goethe-lang/Interpreter.py
+++ goethe-lang/Interpreter.py
@@ -1,67 +1,54 @@
 import sys
 import time
 import random
+import logging
+from typing import Tuple, Callable
+
 from Token import Token
 from Memory import Memory
 from Tokenizer import Tokenizer
 from LanguageTools import LanguageTools
 
 
+class InvalidTokenException(Exception):
+    pass
+
+
 class Interpreter:
-    def __init__(self, text='', lang='de_DE', console_mode=False):
-        self.console_mode = console_mode
+    """The interpreter holds the program status and is responsible for executing the Goethe code.
+    """
+
+    def __init__(self, text='', lang='de_DE', console_mode=True):
+        """
+        Args:
+            text (str, optional): Goethe code. Defaults to ''.
+            lang (str, optional): Language code. Defaults to 'de_DE'.
+            console_mode (bool, optional): Enables or disables console mode. Defaults to True.
+        """
         self.user_input = ''
-
         self.event_listeners = dict()
+        self.console_mode = console_mode
 
         self.tokenizer = Tokenizer(LanguageTools(text, lang))
-
         self.program = self.tokenizer.tokenize()
-        self.pointer = 0
         self.memory = Memory(256)
-
-    def set_text(self, text='', lang='de_DE'):
-        self.tokenizer = Tokenizer(LanguageTools(text, lang))
-        self.program = self.tokenizer.tokenize()
         self.pointer = 0
-        self.memory.reset()
 
-    def _get_next_instruction(self):
-        if self.pointer >= len(self.program):
-            return None
+    def PASS(self) -> None:
+        """Does nothing.
+        """
 
-        self.pointer += 1
-        return self.program[self.pointer]
+        pass
 
-    def _get_previous_instruction(self):
-        if self.pointer < 0:
-            return None
+    def IF(self) -> None:
+        """Jumps to the appropriate FI command in the program if current memory value == 0.
+        """
 
-        self.pointer -= 1
-        return self.program[self.pointer]
-
-    def _get_current_instruction(self):
-        if self.pointer < len(self.program):
-            return self.program[self.pointer]
-
-        return None
-
-    def CP(self):
-        current_pointer = self.memory._pointer
-
-        pointer_one = self._get_next_instruction()
-
-        self.memory.set_pointer_value(int(self._get_next_instruction()))
-        value = self.memory.get_value()
-        self.memory.set_pointer_value(pointer_one)
-        self.memory.set_value(int(value))
-
-        self.memory.set_pointer_value(current_pointer)
-
-    def IF(self):
         if self.memory.get_value() != 0:
             return
 
+        # Finds the appropriate FI command by counting the
+        # nested IF and FI commands in the program.
         nesting = 1
         while nesting:
             instruction = self._get_next_instruction()
@@ -70,10 +57,15 @@ class Interpreter:
             elif instruction == Token.FI:
                 nesting -= 1
 
-    def FI(self):
+    def FI(self) -> None:
+        """Jumps to the appropriate IF command in the program if current memory value != 0.
+        """
+
         if self.memory.get_value() == 0:
             return
 
+        # Finds the appropriate IF command by counting the
+        # nested IF and FI commands in the program.
         nesting = -1
         while nesting:
             instruction = self._get_previous_instruction()
@@ -82,13 +74,18 @@ class Interpreter:
             elif instruction == Token.FI:
                 nesting -= 1
 
-    def IN(self):
+    def IN(self) -> None:
+        """Reads a single char from stdin or self.user_input.
+        """
+
         if self.console_mode:
+            # Reads input from stdin if console mode is enabled.
             char = ord(sys.stdin.read(1))
-            if char == 10:  # Ascii enter code
+            if char == 10:  # Ascii code for ENTER
                 return
 
         else:
+            # Reads input from self.user_input if console mode is disabled.
             if len(self.user_input):
                 char = ord(self.user_input[0])
                 self.user_input = self.user_input[1:]
@@ -96,70 +93,164 @@ class Interpreter:
                 return
 
         self.memory.set_value(char)
-        self.__dispatch_event('<in>', chr(self.memory.get_value()))
+        self.__dispatch_event('<in>')
 
-    def OUT(self):
+    def OUT(self) -> None:
+        """Writes the current memory value as ASCII to the console.
+        """
+
         if self.console_mode:
             print(chr(self.memory.get_value()), end="", flush=True)
 
         self.__dispatch_event('<out>', chr(self.memory.get_value()))
 
-    def INCPTR(self):
+    def INCPTR(self) -> None:
+        """Increments memory pointer.
+        """
+
         self.memory.increment_pointer()
 
-    def DECPTR(self):
+    def DECPTR(self) -> None:
+        """Decrements memory pointer.
+        """
+
         self.memory.decrement_pointer()
 
-    def INCVAL(self):
+    def INCVAL(self) -> None:
+        """Increments memory value.
+        """
+
         self.memory.increment_value()
 
-    def DECVAL(self):
+    def DECVAL(self) -> None:
+        """Decrements memory value.
+        """
+
         self.memory.decrement_value()
 
-    def RND(self):
+    def RND(self) -> None:
+        """Writes random int to memory.
+        """
+
         value = random.randint(0, 255)
         self.memory.set_value(value)
 
-    def run(self):
+    def set_code(self, text='', lang='de_DE') -> None:
+        """Runs given text through the tokenizer and sets tokens as program.
+
+        Args:
+            text (str, optional): Goethe code. Defaults to ''.
+            lang (str, optional): Language code. Defaults to 'de_DE'.
+        """
+
         self.pointer = 0
         self.memory.reset()
+        self.tokenizer = Tokenizer(LanguageTools(text, lang))
+        self.program = self.tokenizer.tokenize()
+
+    def set_user_input(self, input: str):
+        """Sets the user input to the given text. Useful when console mode is disabled.
+
+        Args:
+            input (str): User input.
+        """
+
+        self.user_input = input
+
+    def _get_next_instruction(self) -> Tuple[Token, None]:
+        """Returns next program instruction and increments program pointer.
+
+        Returns:
+            Token: Next program token.
+        """
+
+        if self.pointer >= len(self.program):
+            # No further instruction.
+            return None
+
+        self.pointer += 1
+        return self.program[self.pointer]
+
+    def _get_previous_instruction(self) -> Tuple[Token, None]:
+        """Returns previous program instruction and decrements program pointer.
+
+        Returns:
+            Token: Previous program token.
+        """
+
+        if self.pointer < 0:
+            # No previous instruction.
+            return None
+
+        self.pointer -= 1
+        return self.program[self.pointer]
+
+    def _get_current_instruction(self) -> Tuple[Token, None]:
+        """Returns current program instruction.
+
+        Returns:
+            Token: Current program token.
+        """
+
+        if self.pointer < len(self.program):
+            return self.program[self.pointer]
+
+        return None
+
+    def run(self) -> None:
+        """Executes the program from the current position to the end.
+        """
 
         while self._get_current_instruction():
             self.step()
 
-        self.step()  # Last step, program has reached the end
+        self.step()  # Last step, the program has reached the end.
 
-    def step(self):
+    def step(self) -> None:
+        """Executes the current command in the program.
+        """
+
         instruction = self._get_current_instruction()
 
         if instruction == None:
+            # The end of the program has been reached.
             self.pointer = 0
             self.memory.reset()
             self.__dispatch_event('<end>')
             return
 
         if hasattr(self, instruction.name):
+            # Calls the function matching the token.
             getattr(self, instruction.name)()
+        else:
+            raise InvalidTokenException
+
         self.pointer += 1
         self.__dispatch_event('<step>')
 
-    def add_event_listener(self, type, listener):
+    def add_event_listener(self, type: str, listener: Callable[[str], None]) -> None:
+        """Registers event listener that is called when the given event type occurs.
+
+        Args:
+            type (str): Event type signature.
+            listener (Callable[[str], None]): Event listener to be called.
+        """
+
         if type not in self.event_listeners:
             self.event_listeners[type] = []
 
         self.event_listeners[type].append(listener)
 
-    def __dispatch_event(self, type, value=False):
+    def __dispatch_event(self, type: str, value=None) -> None:
+        """Calls all event listeners for the specified event type.
+
+        Args:
+            type (str): Event type signature.
+            value (str, optional): Data to be passed to the listener.. Defaults to None.
+        """
         if type in self.event_listeners:
             for event in self.event_listeners[type]:
-                event(value)
-
-    def set_user_input(self, input):
-        self.user_input = input
-
-
-if __name__ == "__main__":
-    argv = sys.argv[1:]
-
-    i = Interpreter()
-    i.run()
+                try:
+                    event(value)
+                except:
+                    logging.error('Event listener could not be called.')
