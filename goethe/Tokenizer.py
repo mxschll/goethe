0fe93cfdca763e6f4febd0ad30d3249cdf5a5e4c
Maximilian Schöll <max@schoell.co>
2021-06-02 15:47:03 +0200

    doc: comment functions

--- goethe/Tokenizer.py
+++ goethe/Tokenizer.py
@@ -3,33 +3,54 @@ from Token import Token
 
 
 class Tokenizer:
-    def __init__(self, language: LanguageTools):
-        self.language = language
+    """Creates a list of tokens based on the syllables and stylistic elements of a text.
+    """
+
+    def __init__(self, language_tools: LanguageTools):
+        """
+        Args:
+            language_tools (LanguageTools): A LanguageTools instance.
+        """
+
         self.__program = []
+        self.__lt = language_tools
 
-        self.syllables = self.language.count_syllables_in_lines()
-        alliterations = self.language.find_alliteration()
-        epistrophe = self.language.find_epistrophe()
-        assonance = self.language.find_assonance()
-        anaphora = self.language.find_anaphora()
+        self.syllables = self.__lt.count_syllables_in_lines()
+        alliterations = self.__lt.find_alliteration()
+        epistrophe = self.__lt.find_epistrophe()
+        assonance = self.__lt.find_assonance()
+        anaphora = self.__lt.find_anaphora()
 
-        # Removes epistrophe that overlap with anaphora
+        # Removes epistrophe that overlap with anaphora.
         epistrophe = [e for e in epistrophe if e not in
                       list(set(anaphora).intersection(epistrophe))]
 
         for verse in alliterations:
             self.syllables[verse] = 7
 
+        for verse in assonance:
+            self.syllables[verse] = 9
+
+        # Adds the syllable numbers of consecutive verses with anaphors.
         for verse in anaphora[::-1]:
             self.syllables[verse[0]] += self.syllables[verse[1]]
             del self.syllables[verse[1]]
 
+        # Subtracts the syllable numbers of consecutive verses with anaphors.
         for verse in epistrophe[::-1]:
             self.syllables[verse[0]] -= self.syllables[verse[1]]
             del self.syllables[verse[1]]
 
-        self.syllables = ''.join(map(str, self.syllables))
-        self.syllables = [int(char) for char in self.syllables]
+        self.syllables = map(abs, self.syllables)
+
+    def __append_token(self, token: Token) -> None:
+        """Appends the given token to the program.
+
+        Args:
+            token (Token): Token to be appended.
+        """
+
+        self.__program.append(token)
 
     def tokenize(self) -> list:
         """Converts syllables into a list of program tokens.
@@ -43,12 +64,3 @@ class Tokenizer:
                 self.__append_token(Token(element % 10))
 
         return self.__program
-
-    def __append_token(self, token: Token) -> None:
-        """Appends the given token to the program.
-
-        Args:
-            token (Token): Token to be appended.
-        """
-
-        self.__program.append(token)
